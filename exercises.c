cff45b696cf656a16a3fe34c4b25aacc6163cba3
Replit user <>
2023-03-19 00:47:31 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -133,6 +133,5 @@ void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
   asignarValor(vectorB , 0 , b1);
   asignarValor(vectorB , 1 , b2);
 
-  c->datos[0] = vectorA->datos[0] + vectorB->datos[0];
-  c->datos[1] = vectorA->datos[1] + vectorB->datos[1];
+  sumaV(vectorA, vectorB, c);
 }
