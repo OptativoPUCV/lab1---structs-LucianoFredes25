73be782ff48cf2de60d099e54d4356fe85eff0d4
Replit user <>
2023-03-19 00:30:15 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -127,8 +127,8 @@ sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
   int datos = 2;
-  Vector vectorA = crearVector(datos);
-  Vector vectorB = crearVector(datos);
+  Vector *vectorA = crearVector(datos);
+  Vector *vectorB = crearVector(datos);
 
   asignarValor(*vectorA , 0 , a1);
   asignarValor(*vectorA , 1 , a2);
